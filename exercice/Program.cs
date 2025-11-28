b7ae736d55014d52d0155c9587a36715d09be553
alisql2 <alisqualli@hotmail.com>
2025-11-28 13:43:53 -0500

    3 -ème commit pour modifier le contenu du fichier test
    Ce commit permet de modifier le contenu du fichier test dans  branche 1  et la pusher online

--- exercice/Program.cs
+++ exercice/Program.cs
@@ -4,10 +4,14 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Pieds : ");
+        // Instruction plus claire
+        Console.Write("Saisissez une valeur en Pieds : ");
         double pieds = double.Parse(Console.ReadLine());
         double metresPieds = PiedsEnMetres(pieds);
-        Console.WriteLine("En mètres = " + metresPieds);
+        // Expression du resultat en phrase plus claire
+        Console.WriteLine("La valeur " + pieds + " équivaux à " + metresPieds + " En mètres  ");
+        //Ajout de cette ligne pour demontrer que cela vient du commit1
+        Console.WriteLine(" branche1");
 
     }
 
