618625fea12c65e4aedc00f8ba9b48300c498e9b
alisql2 <alisqualli@hotmail.com>
2025-11-28 12:44:22 -0500

    1 er commit pour copier le contenu dans le main du git
    Notre premier commit pour mettre le contenu qui se trouve en local dans le main

---
+++ exercice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Pieds : ");
+        double pieds = double.Parse(Console.ReadLine());
+        double metresPieds = PiedsEnMetres(pieds);
+        Console.WriteLine("En mètres = " + metresPieds);
+
+    }
+
+    static double PiedsEnMetres(double p)
+    {
+        return p * 0.3048;
+    }
+
+}
